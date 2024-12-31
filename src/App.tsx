eb2f5a7ba9dee6717e582a21d637a63cb619166f
NadiiaSka <syvakivska@gmail.com>
2024-12-31 13:59:50 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="text-7xl font-bold">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+export default App;
