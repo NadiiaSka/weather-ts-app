745ec0754e44119ac51dd9b31d5769ea73948a6c
NadiiaSka <syvakivska@gmail.com>
2025-01-04 11:11:32 +0200

    add week days helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,1 +1,18 @@
 export const roundUp = (num: number): number => Math.ceil(num);
+
+const WEEK_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+// create a list of week days starting from the current day
+const dayInAWeek = new Date().getDay();
+export const forecastWeekDays = WEEK_DAYS.slice(
+  dayInAWeek,
+  WEEK_DAYS.length
+).concat(WEEK_DAYS.slice(0, dayInAWeek));
