c5b48a9dee0049fc269626d8e5c71915e48c41fd
NadiiaSka <syvakivska@gmail.com>
2025-02-25 21:15:50 +0200

    add one more line to the skeleton layout

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,7 +5,7 @@ function Loading() {
     <div className="items-center flex flex-col">
       <Skeleton className="w-[100%] h-[40px] bg-white rounded-lg mt-5" />
       <Skeleton className="max-w-[100%] w-[350px] h-[220px]  bg-white  rounded-xl my-6" />
-      {Array.from({ length: 4 }).map((_, index) => {
+      {Array.from({ length: 5 }).map((_, index) => {
         return (
           <Skeleton
             key={index}
